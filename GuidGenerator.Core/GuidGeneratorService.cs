023cb54095c5ffe0b2564d821be2b0c76be5a536
Ghani Malik <ghani.malik@asml.com>
2026-03-09 11:44:46 +0100

    make a nupkg and restructure code project

---
+++ GuidGenerator.Core/GuidGeneratorService.cs
@@ -0,0 +1,26 @@
+﻿namespace GuidGenerator;
+
+public class GuidGeneratorService
+{
+    /// <summary>
+    /// Generates a specified number of GUIDs.
+    /// </summary>
+    /// <param name="count">The number of GUIDs to generate.</param>
+    /// <returns>A list of generated GUIDs.</returns>
+    /// <exception cref="ArgumentException">Thrown when count is less than or equal to zero.</exception>
+    public List<Guid> GenerateGuids(int count)
+    {
+        if (count <= 0)
+        {
+            throw new ArgumentException("Count must be greater than zero.", nameof(count));
+        }
+
+        var guids = new List<Guid>(count);
+        for (int i = 0; i < count; i++)
+        {
+            guids.Add(Guid.NewGuid());
+        }
+
+        return guids;
+    }
+}
